7eb7ac5e3ad56033a0abde2ca15a606ac46a6164
pridato <davidarroyo25052004@gmail.com>
2024-06-25 11:58:28 +0200

    pendiente mejorar cards con svgs...

--- to-do-app-front/src/app/components/tasks/card-task.tsx
+++ to-do-app-front/src/app/components/tasks/card-task.tsx
@@ -13,7 +13,27 @@ const CardTask: React.FC<CardTaskProps> = ({ task }) => {
           <button className="border rounded-md w-[5vw] text-sm px-3 py-1 hover:bg-gray-200 border-gray-300 flex items-center justify-center gap-2">
             <span className="text-xs">img</span>
             <span className="text-gray-400">Hoy</span>
-            <button>X</button>
+            <button className="text-xs">X</button>
+          </button>
+        )
+      case TaskKeys.Priority:
+        return (
+          <button className="border rounded-md w-[1vw] text-sm px-3 py-1 hover:bg-gray-200 border-gray-300 flex items-center justify-center gap-2">
+            <span className="text-gray-400">B</span>
+          </button>
+        )
+      case TaskKeys.Reminders:
+        return (
+          <button className="border rounded-md w-[8vw] text-sm px-3 py-1 hover:bg-gray-200 border-gray-300 flex items-center justify-center gap-2">
+            <span className="text-xs">img</span>
+            <span className="text-gray-400 text-xs">ACTUALIZAR</span>
+          </button>
+        )
+      case TaskKeys.Tags:
+        return (
+          <button className="border rounded-md w-[8vw] text-sm px-3 py-1 hover:bg-gray-200 border-gray-300 flex items-center justify-center gap-2">
+            <span className="text-xs">img</span>
+            <span className="text-gray-400 text-xs">Etiquetas</span>
           </button>
         )
     }
--- to-do-app-front/src/app/components/tasks/formTask.tsx
+++ to-do-app-front/src/app/components/tasks/formTask.tsx
@@ -25,8 +25,16 @@ const FormTasks: React.FC<formTasksProps> = ({ showTaskForm, setShowTaskForm }) 
     <div className="mt-4 border rounded-md shadow-sm w-[20vw] px-4 py-2">
       <input ref={inputRef} type="text" placeholder="Nombre de la tarea" className="text-xl text-gray-400 font-semibold focus:outline-none" />
       <input type="text" placeholder="Descripción" className=" text-gray-400 text-sm focus:outline-none" />
-      {/** card fecha vencimiento */}
-      <CardTask task={TaskKeys.DueDate} />
+      {/** card fecha vencimiento y prioridad*/}
+      <div className="flex items-start justify-start gap-3">
+        <CardTask task={TaskKeys.DueDate} />
+        <CardTask task={TaskKeys.Priority} />
+      </div>
+      {/** card recordatorios y etiquetas */}
+      <div className="flex items-start justify-start gap-3 mt-2">
+        <CardTask task={TaskKeys.Reminders} />
+        <CardTask task={TaskKeys.Tags} />
+      </div>
     </div>
   )
 }
