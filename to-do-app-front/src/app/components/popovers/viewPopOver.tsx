bf3392a88aa12d2036406ae47db3bc67b80a2bc6
pridato <davidarroyo25052004@gmail.com>
2024-06-27 15:58:24 +0200

    popover vista terminado ordenado

--- to-do-app-front/src/app/components/popovers/viewPopOver.tsx
+++ to-do-app-front/src/app/components/popovers/viewPopOver.tsx
@@ -1,4 +1,5 @@
-import { PopoverContent, PopoverArrow, PopoverHeader, PopoverBody, PopoverFooter } from "@chakra-ui/react";
+import { PopoverContent, PopoverArrow, PopoverHeader, PopoverBody, PopoverFooter, Tooltip } from "@chakra-ui/react";
+import { IconChevronDown } from '@tabler/icons-react';
 
 const ViewPopOver = () => {
   return (
@@ -7,7 +8,21 @@ const ViewPopOver = () => {
       <PopoverArrow />
       <PopoverHeader >
         <div className="flex flex-col">
+          <div className="flex items-center justify-between">
           <h5 className="font-semibold">Vista</h5>
+          {/** interrogante sobre vista */}
+          <Tooltip label="La vista se sincroniza entre compañeros de equipo en proyectos compartidos." placement='top-start'>
+            <button>
+              <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-circle-question stroke-[#6f6a6a] hover:stroke-[#422a2a]" width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <circle cx="12" cy="12" r="9" />
+                <line x1="12" y1="17" x2="12" y2="17.01" />
+                <path d="M12 12v5" />
+                <path d="M12 7v.01" />
+              </svg>
+            </button>
+          </Tooltip>
+          </div>
           {/** configuración de como ver la vista actual */}
           <div className="flex items-center justify-center gap-1 py-1 px-2 mt-2 rounded-md bg-gray-100">
             {/** elemento lista */}
@@ -26,12 +41,76 @@ const ViewPopOver = () => {
       </PopoverHeader>
       <PopoverBody>
         <div>
-          <h5>Ordenar por</h5>
+          {/** header del body */}
+          <div className="flex flex-col">
+            <div className="flex items-center justify-between">
+            <h5 className="font-semibold">Ordenar por</h5>
+            {/** interrogante sobre vista */}
+            <Tooltip label="Las opciones para ordenar se te aplican solo a ti." placement='top-start'>
+              <button>
+                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-circle-question stroke-[#6f6a6a] hover:stroke-[#422a2a]" width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <circle cx="12" cy="12" r="9" />
+                  <line x1="12" y1="17" x2="12" y2="17.01" />
+                  <path d="M12 12v5" />
+                  <path d="M12 7v.01" />
+                </svg>
+              </button>
+            </Tooltip>
+            </div>
+            
+          </div>
+
+          {/** propio cuerpo */}
+          <div className="mt-2 text-sm">
+            {/** opcion agrupacion */}
+            <button className="flex items-center justify-between px-1 py-1 w-full rounded-md hover:bg-gray-100">
+              <div className="flex items-center justify-center gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" aria-hidden="true"><path fill="currentColor" fillRule="evenodd" d="M18 3a3 3 0 0 1 3 3v12a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3V6a3 3 0 0 1 3-3h12Zm0 1H6a2 2 0 0 0-1.995 1.85L4 6v12a2 2 0 0 0 1.85 1.994L6 20h12a2 2 0 0 0 1.994-1.85L20 18V6a2 2 0 0 0-1.85-1.995L18 4Zm-3 4.5A1.5 1.5 0 0 0 13.5 7h-5A1.5 1.5 0 0 0 7 8.5v5A1.5 1.5 0 0 0 8.5 15h5a1.5 1.5 0 0 0 1.5-1.5v-5ZM8.5 8h5l.09.008A.5.5 0 0 1 14 8.5v5l-.008.09a.5.5 0 0 1-.492.41h-5l-.09-.008A.5.5 0 0 1 8 13.5v-5l.008-.09A.5.5 0 0 1 8.5 8Zm.585 8a1.5 1.5 0 0 0 1.415 1h5a1.5 1.5 0 0 0 1.5-1.5v-5a1.5 1.5 0 0 0-1-1.415V15.5a.5.5 0 0 1-.5.5H9.085Z" clipRule="evenodd"></path></svg>
+                <span>Agrupar por</span>
+              </div>
+              <div className="text-gray-500 flex items-center justify-center gap-1 hover:text-black">
+                <span >Ninguno</span>
+                {/** svg flecha hacia abajo */}
+                <IconChevronDown stroke={1.5} />
+              </div>
+            </button>
+
+            {/** opcion ordenado */}
+            <button className="flex items-center justify-between px-1 py-1 w-full rounded-md hover:bg-gray-100">
+              <div className="flex items-center justify-center gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" aria-hidden="true"><path fill="currentColor" d="m16.854 5.146 3 3a.502.502 0 0 1-.708.708L17 6.707V18.5a.5.5 0 0 1-1 0V6.707l-2.146 2.147a.502.502 0 0 1-.708-.708l3-3a.502.502 0 0 1 .708 0zM7.5 5a.5.5 0 0 1 .5.5v11.791l2.146-2.145a.502.502 0 0 1 .708.708l-3 3a.502.502 0 0 1-.708 0l-3-3a.502.502 0 0 1 .708-.708L7 17.293V5.5a.5.5 0 0 1 .5-.5z"></path></svg>
+                <span>Ordenar por</span>
+              </div>
+              <div className="text-gray-500 flex items-center justify-center gap-1 hover:text-black">
+                <span className="w-[80%] truncate">Inteligente </span>
+                {/** svg flecha hacia abajo */}
+                <IconChevronDown stroke={1.5} />
+              </div>
+            </button>
+          </div>
         </div>
+        
       </PopoverBody>
       <PopoverFooter>
-        <div>
-          <h5>Filtrar por</h5>
+        {/** header del body */}
+        <div className="flex flex-col">
+          <div className="flex items-center justify-between">
+          <h5 className="font-semibold">Filtrar por</h5>
+          {/** interrogante sobre vista */}
+          <Tooltip label="Las opciones para filtrar se te aplican solo a ti." placement='top-start'>
+            <button>
+              <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-circle-question stroke-[#6f6a6a] hover:stroke-[#422a2a]" width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <circle cx="12" cy="12" r="9" />
+                <line x1="12" y1="17" x2="12" y2="17.01" />
+                <path d="M12 12v5" />
+                <path d="M12 7v.01" />
+              </svg>
+            </button>
+          </Tooltip>
+          </div>
+          
         </div>
       </PopoverFooter>
     </PopoverContent>
